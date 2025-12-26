62620d9fcf464733f62522192186404e86f95233
Antonio <g5gjbx6t6w@privaterelay.appleid.com>
2025-12-26 12:53:49 +0100

    Add files via upload

--- Sources/Atem/Messages/ChangeTransitionKey.swift
+++ Sources/Atem/Messages/ChangeTransitionKey.swift
@@ -1,28 +1,17 @@
 import Foundation
 
 extension Message.Do {
-    public struct ChangeTransitionKey: SerializableMessage {
-        public static let title = Message.Title(string: "CTTp")
+    struct ChangeTransitionKey: SerializableMessage {
+        static let title = "CTKi"
 
-        public let mixEffectIndex: UInt8
-        public let tie: Bool
+        let mixEffectIndex: UInt8
+        let tie: Bool
 
-        public init(mixEffectIndex: UInt8, tie: Bool) {
-            self.mixEffectIndex = mixEffectIndex
-            self.tie = tie
-        }
-
-        public var dataBytes: [UInt8] {
-            .init(unsafeUninitializedCapacity: 64) { buffer, count in
-                buffer[0] = mixEffectIndex
-                buffer[1] = tie ? 1 : 0
-                count = 64
-            }
-        }
-
-        public var debugDescription: String {
-            "Change Transition Key Tie: ME\(mixEffectIndex) = \(tie)"
+        func serialize() -> Data {
+            var data = Data()
+            data.append(mixEffectIndex)
+            data.append(tie ? 1 : 0)
+            return data
         }
     }
 }
-
